21e512b5246e35413b42078f72ac49c90766652b
mahii2807 <readthedocs@outlook.com>
2026-07-10 11:12:16 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,15 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var ze = document.createElement('script');
-    ze.id = 'ze-snippet';
-    ze.src = 'https://static.zdassets.com/ekr/snippet.js?key=7668c976-0fb7-47ee-8740-3e9115bdb10f';
-    document.body.appendChild(ze);
-});
+project = 'TomTom'
+author = 'Your Name'
+release = '1.0'
+
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+html_js_files = [
+    'chatbot.js',
+]
+html_favicon = '.png'
